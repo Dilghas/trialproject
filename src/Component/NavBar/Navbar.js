26300a8628fa7d8e5aae75f9d2054555244a7cf6
Aswin P Das <aswinpdas83@gmail.com>
2022-12-13 10:22:47 +0530

    change Navbar class comp to fun comp

--- src/Component/NavBar/Navbar.js
+++ src/Component/NavBar/Navbar.js
@@ -1,51 +1,33 @@
-import React, { Component } from "react";
-import "./NavBar.css"
-import image from "../Images/empay.png"
+import React from "react";
+import "./NavBar.css";
+import image from "../Images/empay.png";
 import { Link } from "react-router-dom";
-import NavService from "../../Service/NavService";
 
+const menuList = (data) => {
+  return data.map(({ title, page }, index) => {
+    return (
+      <li key={index}>
+        <Link to={`${page}`}>{title}</Link>
+      </li>
+    );
+  });
+};
 
-
-class NavBar extends Component {
-    state = {
-        data: []
-    }
-    menuList = () => {
-        return (this.state.data.map(({ title, page }, index) => {
-            return (
-                <li key={index}>
-                    <Link to={`${page}`}>{title}</Link>
-                </li>
-            )
-        }))
-    }
-
-    componentDidMount() {
-        this.props.data.then((data) => {
-            this.setState({ data });
-        });
-    }
-    render() {
-
-        return (
-            <nav>
-                <div className="logo">
-                    <img src={image} alt="logo" />
-                </div>
-                <div>
-
-                    <ul className="menu-list">
-                        <li>
-                            <Link to={"/"}>Home</Link>
-                        </li>
-                        {this.menuList()}
-                    </ul>
-                </div>
-
-
-            </nav>
-        )
-    }
-}
-
+const NavBar = (props) => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src={image} alt="logo" />
+      </div>
+      <div>
+        <ul className="menu-list">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          {menuList(props.data)}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 export default NavBar;
