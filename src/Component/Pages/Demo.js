d58a1539e128676cab0bc49ea7c2768b544daa8c
Aswin P Das <aswinpdas83@gmail.com>
2022-12-15 14:33:47 +0530

    Upload and view image

--- src/Component/Pages/Demo.js
+++ src/Component/Pages/Demo.js
@@ -1,22 +1,43 @@
 //import axios from "axios";
 import { Container } from "@mui/system";
-import { Button, Divider, Paper, Toolbar, Typography } from "@mui/material";
+import {
+  Button,
+  CardMedia,
+  Divider,
+  Paper,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const Demo = () => {
   const [image, setImage] = useState("");
 
+  const imageViewHandler = () => {
+    let file = image;
+    setImage(URL.createObjectURL(file));
+    console.log(file);
+  };
+
   const onClickImage = async () => {
     const formData = new FormData();
     formData.append("image", image);
+    console.log(formData);
+
     let result = await fetch("http://localhost:8081/get/csmt/upload", {
       method: "POST",
       body: formData,
     });
-    alert("data has been saved"+result);
+    alert("data has been saved" + result);
+  };
+
+  const imageHandling = (event) => {
+    setImage(event.target.files[0]);
   };
   return (
     <div>
@@ -38,18 +59,30 @@ const Demo = () => {
                   >
                     Upload
                   </Button>
-                  <IconButton
-                    color="primary"
-                    aria-label="upload picture"
-                    component="label"
-                  >
-                    <input
-                      hidden
-                      type="file"
-                      onChange={(e) => setImage(e.target.files[0])}
-                    />
-                    <PhotoCamera />
-                  </IconButton>
+                  <Tooltip title="Select File" arrow>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                    >
+                      <input hidden type="file" onChange={imageHandling} />
+                      {console.log(image)}
+                      <PhotoCamera />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="View" arrow>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                      onClick={imageViewHandler}
+                    >
+                      <VisibilityIcon />
+                    </IconButton>
+                  </Tooltip>
+                  <div className="image">
+                    <CardMedia component="img" image={image} />
+                  </div>
                 </Grid2>
               </Grid2>
             </Grid2>
