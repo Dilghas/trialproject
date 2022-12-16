4a4b0608e03841956ae8c940d6d325320fb71307
Aswin P Das <aswinpdas83@gmail.com>
2022-12-16 18:05:05 +0530

    test

--- src/Component/Pages/FileUpload.js
+++ src/Component/Pages/FileUpload.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 export default class FileUpload extends Component {
+constructor(props){
+  super(props);
+ this.state ={
+    images : undefined,
+    view : ""
+  }
+}
+  
+
+ onChangeImage=(e)=>{
+  console.log(e.target.files[0])
+  const a=  e.target.files[0];
+  this.setState({view : a})
+ }
+
+ onClickButtonView=()=>{
+console.log(this.view)
+ }
 
   render() {
    
     return (
       <div>
-       <h1>File Upload</h1>
+        <input type="file" onChange={this.onChangeImage}/>
+      <button onClick={this.onClickButtonView} >view</button>
       </div>
     )
   }
