91758989d1262ee65890cf111ff4049db9f04af7
Aswin P Das <aswinpdas83@gmail.com>
2022-12-19 18:14:16 +0530

    Message from backent and pop up added

--- src/Component/Pages/Demo.js
+++ src/Component/Pages/Demo.js
@@ -1,9 +1,10 @@
-//import axios from "axios";
 import { Container } from "@mui/system";
 import {
+  Box,
   Button,
   CardMedia,
   Divider,
+  Modal,
   Paper,
   Toolbar,
   Tooltip,
@@ -12,34 +13,54 @@ import {
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
-import FileUploadIcon from "@mui/icons-material/FileUpload";
+import FolderIcon from "@mui/icons-material/Folder";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import axios from "axios";
+import "../Css/Message.css";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Demo = () => {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState(null);
   const [preview, setPreview] = useState();
   const [visibility, setVisibility] = useState(false);
+  const [message, setMessage] = useState(undefined);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const messageAlert = (event) => {};
 
   const imageViewHandler = () => {
     setVisibility(!visibility);
-    setPreview(image !== "" ? URL.createObjectURL(image) : image);
+    setPreview(image !== null ? URL.createObjectURL(image) : image);
   };
 
   const onClickImage = async () => {
-    if(image!==""){
     const formData = new FormData();
     formData.append("image", image);
-
-    let result = await fetch("http://localhost:8081/get/csmt/upload", {
-      method: "POST",
-      body: formData,
-    });
-    const data= JSON.stringify(result);
-    console.log(data)
-  }else{
-    alert("please upload image")
-  }
+    // let result = await fetch("http://localhost:8081/get/csmt/upload", {
+    //   method: "POST",
+    //   body: formData,
+    // });
+    axios
+      .post("http://localhost:8081/get/csmt/upload", formData)
+      .then((response) => {
+        console.log(response.data);
+        // setMessage(response.data)
+        messageAlert(setMessage(response.data));
+        handleOpen();
+      });
   };
 
   const imageHandling = (event) => {
@@ -64,10 +85,14 @@ const Demo = () => {
                     variant="contained"
                     component="label"
                     onClick={onClickImage}
+                    disabled={!image}
                   >
                     Upload
                   </Button>
-                  <Tooltip title={image ?URL.createObjectURL(image):"Select file"} arrow>
+                  <Tooltip
+                    title={image ? URL.createObjectURL(image) : "Select file"}
+                    arrow
+                  >
                     <IconButton
                       color="primary"
                       aria-label="upload picture"
@@ -79,10 +104,10 @@ const Demo = () => {
                         accept="image/*"
                         onChange={imageHandling}
                       />
-                      <FileUploadIcon />
+                      <FolderIcon />
                     </IconButton>
                   </Tooltip>
-                  <Tooltip title={visibility ?  "Hide" :"View"} arrow>
+                  <Tooltip title={visibility ? "Hide" : "View"} arrow>
                     <IconButton
                       color="primary"
                       aria-label="upload picture"
@@ -97,6 +122,25 @@ const Demo = () => {
                       <CardMedia component="img" image={preview} />
                     )}
                   </div>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        Message
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {message}
+                      </Typography>
+                    </Box>
+                  </Modal>
                 </Grid2>
               </Grid2>
             </Grid2>
